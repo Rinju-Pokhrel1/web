36dbcc0fe8a74fcf490dda92f7b7e7a69c85af14
Rinju Pokhrel1 <rinjupokhrel91@gmail.com>
2025-09-09 08:49:04 +0545

    event in js

---
+++ js/javascript/event&eventbubbling/script.js
@@ -0,0 +1,7 @@
+let button = document.getElementById("btn");
+button.addEventListener("click", () => {
+//     alert("Button Clicked");
+// });
+
+document.querySelector(".box").innerHTML='hello world , how are you?';
+});
