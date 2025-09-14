2f96ebd6ea13ed896b2f01be4ea2e903b016d4dd
Rinju Pokhrel1 <rinjupokhrel91@gmail.com>
2025-09-14 10:37:39 +0545

    common js vs Ecmascript

---
+++ js/Backend/commonjs vs Ecmascript/main.js
@@ -0,0 +1,24 @@
+
+// const http = require('node:http');
+// const fs =require("fs")
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('"Hello Everyone .I am doing my 100 days of challenge. \n');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+// import http from "http"
+// import {a} from "./mymodule.js"
+// console.log(a)
+
+// for Object
+// import obj from "./mymodule.js"
+// console.log(obj)
+import Rinju from "./mymodule.js"
+console.log(Rinju)
