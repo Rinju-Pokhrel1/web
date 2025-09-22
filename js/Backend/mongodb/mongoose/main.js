91cde1ddf6c80c923ed8abe8bcc62194feaa5b98
Rinju Pokhrel1 <rinjupokhrel91@gmail.com>
2025-09-22 23:33:15 +0545

    mongoose

--- js/Backend/mongodb/mongoose/main.js
+++ js/Backend/mongodb/mongoose/main.js
@@ -3,7 +3,21 @@ import mongoose from "mongoose";
 import express from "express";
 import {  Todo } from "./models/Todo.js";
 
-let conn = await mongoose.connect("mongodb://localhost:27017/todo")
+async function connectDB() {
+  try {
+    const conn = await mongoose.connect("mongodb://localhost:27017/todo", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
+    process.exit(1); // Exit process with failure
+  }
+}
+
+connectDB();
 const app = express()
 const port = 3000
 
@@ -13,12 +27,21 @@ app.get('/', (req, res) => {
     todo.save() 
     res.send('Hello World!')
 })
+app.get("/todo", async (req, res) => {
+  try {
+    const todo = await Todo.findOne({}); // Fetch one document
+    if (!todo) {
+      return res.status(404).json({ message: "No todo found" });
+    }
+    console.log(todo);
+    if(!todo){ res.send("No record found in the database") }
+    res.json({ title: todo.title, desc: todo.desc });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+});
 
-app.get('/todo', async (req, res) => { 
-    let todo = await Todo.findOne({})
-    console.log(todo)
-    res.json({title:todo.title, desc:todo.desc})
-})
 
 
 
--- js/Backend/mongodb/mongoose/models/Todo.js
+++ js/Backend/mongodb/mongoose/models/Todo.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const TodoSchema = new mongoose.Schema({
-    title: {type: String, required: true, default: "Hey"},
-    desc: String,
-    isDone: Boolean,
-    days: Number
+  title: { type: String, required: false },
+  desc: { type: String, required: false },
+  isDone: { type: Boolean, default: false },
+  days: { type: Number, default: 0 }
 });
 
-export const Todo = mongoose.model('Todo', TodoSchema);
+export const Todo = mongoose.model("Todo", TodoSchema);
