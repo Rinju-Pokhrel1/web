f98f631624950af47e8f31ef7f72457044f86391
Rinju Pokhrel1 <rinjupokhrel91@gmail.com>
2025-09-29 21:37:25 +0545

    hooks and state

--- js/Backend/props/first-app/src/App.jsx
+++ js/Backend/props/first-app/src/App.jsx
@@ -1,6 +1,7 @@
-import Footer from "./components/Footer"
-import Navbar from "./components/Navbar"
-import Card from "./components/Card"
+import Footer from "./components/Footer.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Card from "./components/Card.jsx";
+
 
 function App() {
 
@@ -8,10 +9,10 @@ function App() {
     <>
       <Navbar />
       <div className="cards">
-        <Card title="card 1" description="card 1 desc" />
-        <Card title="card 2" description="card 2 desc" />
-        <Card title="card 3" description="card 3 desc" />
-        <Card title="card 4" description="card 4 desc" />
+        <Card title="HTML" description="backbone of the web" />
+        <Card title="css" description="design layer of the web" />
+        <Card title="JS" description="makes web pages interactive." />
+        <Card title="React" description="creates dynamic, scalable, and efficient UIs with reusable components." />
       </div>
       <Footer />
     </>
